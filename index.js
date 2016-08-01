1b39b365207ab608f8124749b6dd270b5f9afa4c
Andrew Oh <andrewoh531@gmail.com>
2016-08-01 20:50:59 +1000

    Changing index.js into a simple export = require

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var createToken = require('./src/index').createToken;
-var isTokenValid = require('./src/index').isTokenValid;
-
-exports.default = {
-  createToken: createToken,
-  isTokenValid: isTokenValid
-};
+module.exports = require('./lib/index');
